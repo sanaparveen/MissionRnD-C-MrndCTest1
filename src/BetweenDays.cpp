43b00b1eca8a4a528895a47f790e46eafdd327e2
sanaparveen <sanaparveen@missionrnd.com>
2016-04-02 20:58:42 +0530

    Submit Commit

--- src/BetweenDays.cpp
+++ src/BetweenDays.cpp
@@ -29,6 +29,81 @@ struct node{
 };
 
 
+int fourDigit(struct node *date);
+int twoDigit(struct node *date);
+
+const int noOfMonthDays[12] = { 31, 28, 31, 30, 31, 30,
+31, 31, 30, 31, 30, 31 };
+int countLeapYear(int mon, int years)
+{
+	if (mon <= 2)
+		years--;
+	return (years / 4) - (years / 100) + (years / 400);
+}
+
+int dateDifference(int day1, int mon1, int year1, int day2, int mon2, int year2)
+{
+
+	int index;
+	long int d1, d2;
+	d1 = year1 * 365 + day1;
+	for (index = 0; index < mon1 - 1; index++)
+		d1 += noOfMonthDays[index];
+	d1 += countLeapYear(mon2, year2);
+
+	d2 = year2 * 365 + day2;
+	for (index = 0; index<mon2 - 1; index++)
+		d2 += noOfMonthDays[index];
+	d2 += countLeapYear(mon2, year2);
+
+	return (d2 - d1) - 1;
+}
+
 int between_days(struct node *date1head, struct node *date2head){
-	return -1;
+
+	int day1 = 0, day2 = 0, mon1 = 0, mon2 = 0, year1 = 0, year2 = 0, index, diff = 0;
+	if (date1head == NULL || date2head == NULL)
+		return -1;
+
+	day1 = twoDigit(date1head);
+	date1head = date1head->next->next;
+	mon1 = twoDigit(date1head);
+	date1head = date1head->next->next;
+	year1 = fourDigit(date1head);
+	day2 = twoDigit(date2head);
+	date2head = date2head->next->next;
+	mon2 = twoDigit(date2head);
+	date2head = date2head->next->next;
+	year2 = fourDigit(date2head);
+
+	if (day1 < 1 || day2 < 1 || mon1 < 1 || mon2 < 1 || year1 < 0 || year2 < 0)
+		return -1;
+	if (day1 > 31 || day2 >  31 || mon1 > 12 || mon2 > 12 || year1 > 9999 || year2 > 9999)
+		return -1;
+
+	diff = dateDifference(day1, mon1, year1, day2, mon2, year2);
+	if (diff < 1)
+		return 0;
+
+	return diff;
+}
+int twoDigit(struct node *date)
+{
+	int val;
+	val = date->data;
+	date = date->next;
+	val = val * 10 + date->data;
+	return val;
+
+}
+int fourDigit(struct node *date)
+{
+	int y = 0;
+	while (date != NULL)
+	{
+		y = y * 10 + date->data;
+		date = date->next;
+	}
+	return y;
+
 }
